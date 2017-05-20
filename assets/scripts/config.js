ceb6224764cbc150a1669919359d35e4c38dc4ef
nriser <riserika@gmail.com>
2017-05-20 11:10:45 -0400

    Initial commit
    Set config for production.

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://mysterious-plains-14810.herokuapp.com',
+    development: 'http://localhost:4741'
+  }
+}
+
+module.exports = config
