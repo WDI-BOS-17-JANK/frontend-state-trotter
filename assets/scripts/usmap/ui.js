411ec13bf430d1559728839c33cb3cba78213cd0
arlofeirman <arlofeirman@gmail.com>
2017-05-23 12:08:26 -0400

    Create form will disappear on success
    Add code to createItemSucess to remove create form from the DOM
    on form submit.
    
    with Nikki Riser -- @nriser

--- assets/scripts/usmap/ui.js
+++ assets/scripts/usmap/ui.js
@@ -13,16 +13,18 @@ const ui = require('./ui')
 // const showMapTemplate = require('../templates/map.handlebars')
 const showStateAllTemplate = require('../templates/state-all-items.handlebars')
 const showStateItemCreateTemplate = require('../templates/state-item-create.handlebars')
-// const showStateItemDetailsTemplate = require('../templates/state-item-details.handlebars')
-// const showStateItemUpdateTemplate = require('../templates/state-item-update.handlebars')
 
-const getItemsSuccess = (data, region) => {
-  // debugger
+const showStateView = () => {
   $('#state-view').append(showStateAllTemplate)
+}
+const showCreateform = () => {
   $('#state-view').append(showStateItemCreateTemplate)
   $('#create-item').on('submit', onCreateItem)
-  // console.log(data)
-  // console.log('inside getItemsSucces region is', region)
+}
+
+const getItemsSuccess = (data, region) => {
+  showStateView()
+  showCreateform()
   const result = data.items.filter((item) => {
     return item.state === region
   })
@@ -61,6 +63,8 @@ const getItemsFailure = (data) => {
 
 const createItemSuccess = (data) => {
   console.log('response is', data)
+  // disappear form on sucess
+  $('#create-item').empty()
 }
 
 const createItemFailure = (data) => {
